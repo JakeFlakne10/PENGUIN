f1ad14bf9d821dec0c0f293c23aaea571c633955
jakeFlak <113362895+JakeFlakne10@users.noreply.github.com>
2022-10-05 09:42:59 -0500

    l

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 penguin = input("1.king\n2.emperor\n3.chinstrap\n4.gentoo", "5.little", "6.african", "7.southern rockhopper", "8.macaroni")
 options["habitat","diet","appearance","conservation","status"]
 myList[int(penguin)]
+
+penguin[]
+habitat["Sub Antartic", ""]
+diet[]
+appearance[]
+conservation[]
+status[]
+
+p =  ("pick a penguin.\n1.king\n2.emperor\n3.chinstrap\n4.gentoo\n5.little\n6.african\n7.southern rockhopper\n 8.macaroni ")
+
+print(penguin[p])
+print(habitat[p])
+print(diet[p])
+print(appearence[p])
+print(status[p])
