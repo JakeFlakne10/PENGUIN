1d947ad494c1cc55e5dfb06ba334358e87ae2bd5
jakeFlak <113362895+JakeFlakne10@users.noreply.github.com>
2022-10-05 10:34:41 -0500

    comments

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-#
-penguin["", "", "", "", "", "", "", ""]
-#
+# 1 2 3 4 5 6 7 8
 habitat["", "", "", "", "", "", "", ""]
-#
+# 1 2 3 4 5 6 7 8
 diet["", "", "", "", "", "", "", ""]
-#
+# 1 2 3 4 5 6 7 8
 appearance["", "", "", "", "", "", "", ""]
-#
+# 1 2 3 4 5 6 7 8
 conservation["", "", "", "", "", "", "", ""]
-#
+# 1 2 3 4 5 6 7 8
 status["", "", "", "", "", "", "", ""]
 
+# choose path/penguin
 p =  ("pick a penguin to learn about.\n1.king\n2.emperor\n3.chinstrap\n4.gentoo\n5.little\n6.african\n7.southern rockhopper\n 8.macaroni ")
 
+#print info based off path
 print(int(penguin[p]))
 print(int(habitat[p]))
 print(int(diet[p]))
 print(int(appearence[p]))
 print(int(status[p]))
+
+# if statements: {0}
+# elif statements: {0}
+# else statements: {0}
+# lines: 25 - 11(comments) = {14}
