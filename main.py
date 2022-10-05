d3f3fdf93b1c790024fe80c9db9ef494048258bf
jakeFlak <113362895+JakeFlakne10@users.noreply.github.com>
2022-10-05 08:59:19 -0500

    layout i think

---
+++ main.py
@@ -0,0 +1,12 @@
+starter["what penguin? King, Emperor, Chinstrap, Gentoo, Little, African, Southern Rockhopper, Macaroni"]
+kingFacts["this, is, stupid"]
+emperorFacts["mid, as, hek"]
+chinstrapFacts[""]
+gentooFacts[""]
+littleFacts[""]
+africanFacts[""]
+southernRockhopperFacts[""]
+macaroniFacts[""]
+def start():
+    if i in range(0,0):
+        print("hi")
