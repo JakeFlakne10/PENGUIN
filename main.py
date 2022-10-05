03b8069db6d8b8f60bba1b935bee399906dcb279
jakeFlak <113362895+JakeFlakne10@users.noreply.github.com>
2022-10-05 10:28:35 -0500

    Completed base(just need text)

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-penguin = input("1.king\n2.emperor\n3.chinstrap\n4.gentoo", "5.little", "6.african", "7.southern rockhopper", "8.macaroni")
-options["habitat","diet","appearance","conservation","status"]
-myList[int(penguin)]
+#
+penguin["", "", "", "", "", "", "", ""]
+#
+habitat["", "", "", "", "", "", "", ""]
+#
+diet["", "", "", "", "", "", "", ""]
+#
+appearance["", "", "", "", "", "", "", ""]
+#
+conservation["", "", "", "", "", "", "", ""]
+#
+status["", "", "", "", "", "", "", ""]
 
-penguin[]
-habitat["Sub Antartic", ""]
-diet[]
-appearance[]
-conservation[]
-status[]
+p =  ("pick a penguin to learn about.\n1.king\n2.emperor\n3.chinstrap\n4.gentoo\n5.little\n6.african\n7.southern rockhopper\n 8.macaroni ")
 
-p =  ("pick a penguin.\n1.king\n2.emperor\n3.chinstrap\n4.gentoo\n5.little\n6.african\n7.southern rockhopper\n 8.macaroni ")
-
-print(penguin[p])
-print(habitat[p])
-print(diet[p])
-print(appearence[p])
-print(status[p])
+print(int(penguin[p]))
+print(int(habitat[p]))
+print(int(diet[p]))
+print(int(appearence[p]))
+print(int(status[p]))
